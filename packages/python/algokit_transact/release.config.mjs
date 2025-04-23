d47f1f3b24ecb6fc99db22aee74df38db552b775
Joe Polny <joepolny@gmail.com>
2025-04-23 10:00:26 -0400

    ci(python): fix artifacts path

--- packages/python/algokit_transact/release.config.mjs
+++ packages/python/algokit_transact/release.config.mjs
@@ -33,7 +33,7 @@ export default {
     [
       "@semantic-release/github",
       {
-        assets: ["artifacts/*-wheel/**/*"],
+        assets: ["../../../artifacts/*-wheel/**/*"],
       },
     ],
   ],
